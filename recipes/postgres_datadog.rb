a75cb3616aa27d497393a049993299ab1b029446
Giovanni Sakti <giosakti@gmail.com>
2018-09-20 15:26:50 +0700

    add recipes for creating process and postgres check

---
+++ recipes/postgres_datadog.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook:: datadog-cookbook
+# Recipe:: postgres_datadog
+#
+# Copyright:: 2018, BaritoLog.
+#
+#
+
+template "/etc/datadog-agent/conf.d/postgres.d/conf.yaml" do
+  source "datadog.postgres.conf.yaml.erb"
+  owner node[cookbook_name]['user']
+  group node[cookbook_name]['user']
+  mode '0644'
+  variables instances: node[cookbook_name]['postgres']['instances']
+  notifies :restart, 'service[datadog-agent]', :delayed
+end
---
+++ recipes/process_datadog.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook:: datadog-cookbook
+# Recipe:: process_datadog
+#
+# Copyright:: 2018, BaritoLog.
+#
+#
+
+template "/etc/datadog-agent/conf.d/process.d/conf.yaml" do
+  source "datadog.process.conf.yaml.erb"
+  owner node[cookbook_name]['user']
+  group node[cookbook_name]['user']
+  mode '0644'
+  variables instances: node[cookbook_name]['process']['instances']
+  notifies :restart, 'service[datadog-agent]', :delayed
+end
