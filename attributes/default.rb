ede90fa33b6225f0d593838a7d52824b6895b291
Puja Pramudya <>
2019-08-29 15:04:33 +0700

    Add dogstatsd config

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,4 @@ default[cookbook_name]['datadog_api_key'] = ENV['BARITO_DATADOG_API_KEY']
 default[cookbook_name]['datadog_hostname'] = ''
 default[cookbook_name]['datadog_config_path'] = '/etc/datadog-agent'
 default[cookbook_name]['datadog_agent_version'] = '6.5.2'
+default[cookbook_name]['datadog_dogstatsd_port'] = '8125'
